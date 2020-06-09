c54072c814fa63c84d00c9ad0e5b0bb1b0b3aa2b
Eric Johnson <ericjohnson.kc@gmail.com>
2020-06-08 20:02:20 -0500

    Did some tinkering.

---
+++ selfStudyLab1/script.js
@@ -0,0 +1,6 @@
+var myBtn = document.getElementById("myButton");
+
+myBtn.onclick = function(){
+    var pTags = document.getElementsByTagName("p");
+    pTags[0].innerHTML = " Hi Mom, Hi Mom, Hi Mom Hi Mom Hi Mom, Hi Mom, Hi Mom Hi Mom Hi Mom, Hi Mom, Hi Mom Hi Mom Hi Mom, Hi Mom, Hi Mom Hi Mom";
+};
--- week-3/change_maker/make_change.js
+++ week-3/change_maker/make_change.js
@@ -2,3 +2,33 @@ var $ = function(id) {
     return document.getElementById(id);
 };
 
+"use strict";  // Following guidance from the text book, strict mode employed below
+
+function makeChange(changeDue){                      // This function will handle the logic to make appropriate change
+    $("quarters").value = parseInt(changeDue / 25);
+    changeDue -= $("quarters").value * 25;
+
+    $("dimes").value = parseInt(changeDue / 10);
+    changeDue -= $("dimes").value * 10;
+
+    $("nickels").value = parseInt(changeDue / 5);
+    changeDue -= $("nickels").value * 5;
+
+    $("pennies").value = parseInt(changeDue / 1);
+    changeDue -= $("pennies").value * 1;
+
+}
+
+var processEntry = function(){                      // This function will provide rudimentary input validation and call the makeChange() function.
+    var changeDue = $("cents").value;
+    if (isNaN(changeDue) || parseFloat(changeDue) < 0 || parseFloat(changeDue) > 99){    // Establishes how invalid input will be handled.
+        alert("Change amount must be a number from 0 to 99");
+    }
+    else{
+        makeChange(parseFloat(changeDue));
+    }
+};
+
+window.onload = function(){                    // The onload() method ensures the DOM is loaded prior to attaching event handler.
+  this.$("calculate").onclick = processEntry;   // Sets p and event handler for the click event, specifies the processEntry() method for handling.
+};
