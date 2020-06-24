b42700e2e46d8fc31e048e4ccebb0a139769c1dd
Eric Johnson <ericjohnson.kc@gmail.com>
2020-06-24 16:44:23 -0500

    Added downloaded week 7 course files to project folder, added comments with author and date info.

---
+++ week-7/password/password.js
@@ -0,0 +1,40 @@
+/********************************
+ *  Author: Eric Johnson
+ *  Date: 6/24/2020
+ * 
+ *  Grantham University
+ *  CS208 Programming in JavaScript
+ * 
+ *  Week 7:
+ *  Control structures, exceptions,
+ *  regular expressions, cookies
+ * ******************************** */
+
+"use strict";
+$(document).ready(function() {
+    var getRandomNumber = function(max) {
+        var random;
+        if (!isNaN(max)) {
+            random = Math.random(); //value >= 0.0 and < 1.0
+            random = Math.floor(random * max); //value is an integer between 0 and max - 1
+            random = random + 1; //value is an integer between 1 and max
+        }
+        return random;
+    };
+    
+    $("#generate").click(function() {
+        $("#password").val( "" ); // clear previous entry
+    
+        var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-+!@";
+        
+    }); // end click()
+    
+    $("#clear").click(function() {
+        $("#num").val( "" );
+        $("#password").val( "" );
+        $("#num").focus();
+    }); // end click()
+    
+    // set focus on initial load
+    $("#num").focus();
+}); // end ready()
