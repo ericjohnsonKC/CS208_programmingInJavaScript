d685243a5fc9d6c1196d52866f086adc62388b95
Eric Johnson <ericjohnson.kc@gmail.com>
2020-07-07 19:29:11 -0500

    Added comments with name and date info per assignment instructions.

--- week-8/image_rollover/jquery.rollover.js
+++ week-8/image_rollover/jquery.rollover.js
@@ -1,3 +1,14 @@
+/**************************************
+ *  Author: Eric Johnson
+ *  Date:   7/7/2020
+ * 
+ *  Grantham University
+ *  CS208 Programming in JavaScript
+ * 
+ *  Week 8 Assignment
+************************************** */
+
+
 (function($){
     $.fn.changeImage = function() {
         return this.each(function() {
