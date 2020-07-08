2533b9794fcab00681d6dc253dc0b5ff1a147b9f
Eric Johnson <ericjohnson.kc@gmail.com>
2020-07-07 20:30:38 -0500

    Completed the assignment, tested in Chromium, is working fine.  Added comments on the code as per assignment instructions.

--- week-8/image_rollover/jquery.rollover.js
+++ week-8/image_rollover/jquery.rollover.js
@@ -12,7 +12,25 @@
 (function($){
     $.fn.changeImage = function() {
         return this.each(function() {
-
+            $(this).find("img").each(function() {   //  Modified this line of code to execute on descendant img elements.
+                // get old and new urls
+                var oldURL = $(this).attr("src");
+                var newURL = $(this).attr("id");
+        
+                // preload images		
+                var rolloverImage = new Image();
+                rolloverImage.src = newURL;
+        
+                // set up event handlers			
+                $(this).hover(
+                    function() {
+                        $(this).attr("src", newURL);
+                    },
+                    function() {
+                        $(this).attr("src", oldURL);
+                    }
+                ); // end hover
+            }); // end each
         });
     };
 })(jQuery);
--- week-8/image_rollover/rollover.js
+++ week-8/image_rollover/rollover.js
@@ -10,23 +10,5 @@
 
 
 $(document).ready(function() {
-    $("#image_rollovers img").each(function() {
-        // get old and new urls
-        var oldURL = $(this).attr("src");
-        var newURL = $(this).attr("id");
-
-        // preload images		
-        var rolloverImage = new Image();
-        rolloverImage.src = newURL;
-
-        // set up event handlers			
-        $(this).hover(
-            function() {
-                $(this).attr("src", newURL);
-            },
-            function() {
-                $(this).attr("src", oldURL);
-            }
-        ); // end hover
-    }); // end each
+    $("#image_rollovers").changeImage();    // Replaced original code file contents with this line, calling the plugin method.
 }); // end ready
